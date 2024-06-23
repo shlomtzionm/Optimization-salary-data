a4ffa72efcd85a8a531f656d096421dc74c356e0
shlomtzionm <shlomtzosh@gmail.com>
2024-06-23 15:59:44 +0300

    23.06

---
+++ script.js
@@ -0,0 +1,20 @@
+let fileInput = document.getElementById('fileInput');
+
+fileInput.addEventListener('change', handleExcel);
+
+function handleExcel(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        console.log(jsonData);
+    };
+
+    reader.readAsArrayBuffer(file);
+}
